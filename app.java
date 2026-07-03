ea0ea6f8e489c032142f07ca58bdac5983ec1583
yash <yashnani0512@gmail.com>
2026-07-03 08:56:57 +0000

    added-new-features

--- app.java
+++ app.java
@@ -2,6 +2,8 @@ public class App {
 	public static void main(string[] args){
 		int a=10, int b=20;
 		system.out.println("The addition is:"+(a+b));
+		system.out.println("The differnece is:"+(a+b));
+		system.out.println("The product is:"+(a*b))
 	}
 }
 
